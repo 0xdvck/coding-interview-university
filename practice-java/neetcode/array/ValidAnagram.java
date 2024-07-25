4bcfeb5de10793053e6eda97adc0d7ce6f55d931
0xdvck <0xdvck@gmail.com>
2024-07-26 05:57:08 +0700

    [Chore] Add neetcode problem

---
+++ practice-java/neetcode/array/ValidAnagram.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> validAnagram(int[] nums, int k) {
+        //Frequency Array
+        //Time Complexity: O(n)
+        //Space Complexity: O(26)
+        if(s.length() != t.length()) return false;
+
+        int[] frequency = new int[26]; 
+
+        for(int i = 0; i < s.length(); i++) {
+            frequency[s.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i < t.length(); i++) {
+            frequency[t.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if(frequency[i] != 0) return false;
+        }
+
+        return true;
+    
+    }
+}
