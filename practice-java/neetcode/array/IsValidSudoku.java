83c3c86a172fab691029098e1c85faa475fc0e84
0xdvck <0xdvck@gmail.com>
2024-07-26 11:11:50 +0700

    [Chore] Add neetcode problem

---
+++ practice-java/neetcode/array/IsValidSudoku.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        //Time complexity: O(n*n)
+        //Space complexity: O(3n*n)
+
+        boolean[][] r=new boolean[9][9];
+        boolean[][] c=new boolean[9][9];
+        boolean[][] subgroups=new boolean[9][9];
+        for(int i=0;i<9;i++) {
+            for(int j=0;j<9;j++) {
+                char currCh=board[i][j];
+                if(currCh=='.')
+                    continue;
+                int n=currCh-'0'-1;
+                int in=(i/3)*3+j/3;
+                if(r[i][n] || c[j][n] || subgroups[in][n])
+                    return false;
+                r[i][n]=true;
+                c[j][n]=true;
+                subgroups[in][n]=true;
+            }
+        }
+        return true;
+    }
+}
