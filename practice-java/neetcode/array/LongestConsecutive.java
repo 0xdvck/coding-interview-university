de0e272144b7980713838b7eb204074686a78ae7
0xdvck <0xdvck@gmail.com>
2024-07-27 07:59:12 +0700

    [Chore] Add neetcode problem

---
+++ practice-java/neetcode/array/LongestConsecutive.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashMap<Integer, Map.Entry<Integer, Integer>> mp = new HashMap<>();
+        HashMap<Integer, Boolean> bl = new HashMap<>();
+        int mx = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if(bl.containsKey(nums[i])) continue;
+            bl.put(nums[i], true);
+
+
+            int l = nums[i];
+            int r = nums[i];
+            if(mp.containsKey(l - 1)) {
+                l = mp.get(l - 1).getKey();
+            }
+
+            if(mp.containsKey(r + 1)) {
+                r = mp.get(r + 1).getValue();
+            }
+
+            mp.put(l, Map.entry(l,r));
+            mp.put(r, Map.entry(l,r));
+            mx = Math.max(mx,r - l + 1);
+        }
+
+
+        return mx;
+    }
+}
